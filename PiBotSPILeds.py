e3f67465e2cacf283836fd89b32dd86ef85466e8
pangolin2000 <44087302+pangolin2000@users.noreply.github.com>
2018-10-31 14:24:39 +0000

    second update

--- PiBotSPILeds.py
+++ PiBotSPILeds.py
@@ -8,9 +8,14 @@ print wiringpi.wiringPiSPISetup(SPIChannel, SPISpeed)
 #buf = str([0x00,0x00,0x00,0x00,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff])  # []
 
 buf = bytearray.fromhex("00 00 00 00 ff ff ff ff ff ff ff ff")
-print buf
+
+data = "00000000ffffffffffffffff"
+bits = ""
+for x in xrange(0, len(data), 2):
+  bits += chr(int(data[x:x+2], 16))
+print bits
 #sendData = str(0x00,0x00,0x00,0x00)
 
-receiveData = wiringpi.wiringPiSPIDataRW(SPIChannel, buf)
+receiveData = wiringpi.wiringPiSPIDataRW(SPIChannel, bits)
 
 print receiveData
